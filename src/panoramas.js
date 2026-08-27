453b25295c4418b2c87b71bd207dc68248326546
Thibaud Guillon <thibaudguillonfx@gmail.com>
2026-08-27 10:44:48 +0200

    Prompt Felt

--- src/panoramas.js
+++ src/panoramas.js
@@ -23,7 +23,10 @@ export const PANORAMAS = {
     //   styleRef: '/panoramas/ref-style-1',
     //   prompt: 'The first image is the scene, the second is the style reference. Replace the face of a character with each provided face, matching the style reference.',
     // },
-    { id: 'bureau-1', label: 'Felt', src: '/panoramas/style-1' },
+    { id: 'bureau-1', label: 'Felt', src: '/panoramas/style-1' ,
+      styleRef: '/panoramas/ref-style_felt' ,
+      prompt : 'adapt the style of the face picture to the Felt style image, integrate the close up face on a character in the image respecting the characters Felt style, must respect Felt eyes style. Use the Felt image as style reference, character must set at the place of one of the characters in the office. Keep structure of the image' ,
+    },
     { id: 'bureau-2', label: 'Comic', src: '/panoramas/style-2' },
     {
       id: 'bureau-3', label: 'Clay', src: '/panoramas/style-clay',
